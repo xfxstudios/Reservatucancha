eee515c397168b93af46a3ad5bed467865a97ed9
xfxstudios <info.fxstudios@gmail.com>
2018-12-08 08:53:57 -0400

    Agregamos la Peticion al Servidor
    Agregamos la petición al servidor para que retorne la lista de datos a agregar en nuestro adaptador

--- app/src/main/java/proyecto/jean/reservatucancha/Main3Activity_cancha.java
+++ app/src/main/java/proyecto/jean/reservatucancha/Main3Activity_cancha.java
@@ -6,12 +6,7 @@ import android.widget.ListView;
 
 public class Main3Activity_cancha extends AppCompatActivity {
    ListView lista;
-    String[][] datos={
-            {"titulo","information","image_cancha"},
-            {"titulo","information","image_cancha"},
-
-
-    };
+    String[][] datos;
     int[] datosImg={R.drawable.cancha,R.drawable.a};
 
 
@@ -20,8 +15,39 @@ public class Main3Activity_cancha extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3_cancha);
          lista = (ListView) findViewById(R.id.LvLista);
+       
+       RequestQueue queue = Volley.newRequestQueue(this);
+            JsonObjectRequest jobReq = new JsonObjectRequest(Request.Method.POST, "AQUI LA URL", jObject,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject jsonObject) {
 
-        lista.setAdapter(new adaptador(this, datos, datosImg));
+                           JSONArray array = new JSONArray();
+
+                              for(int i=0;i<jsonObject.size();i++){
+                                  JSONObject obj=new JSONObject();
+                                  try {
+                                      obj.put("clave",jsonObject.get(i));
+                                  } catch (JSONException e) {
+                                      e.printStackTrace();
+                                  }
+                                  datos.put(obj);
+                              }
+
+                           lista.setAdapter(new adaptador(this, datos, datosImg));
+
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError volleyError) {
+
+                        }
+                    });
+
+            queue.add(jobReq);
+
+        
 
 
     }
